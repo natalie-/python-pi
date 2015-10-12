6b51fd25585f75748945c7f4cb71105a7958fb02
natalie- <natalie.somersall@gmail.com>
2015-10-12 12:58:10 -0400

    Add whitespace to please flake8 and make it look pretty.

--- 8-led-gui.py
+++ 8-led-gui.py
@@ -23,10 +23,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)   # Silence cleanup warnings if CTRL-C is pressed
 
 # Set up controls
-GPIO.setup([17,18,27,22,23,24,25,4],GPIO.OUT)
+GPIO.setup([17, 18, 27, 22, 23, 24, 25, 4], GPIO.OUT)
 
 # Turn all LEDs off
-GPIO.output([17,18,27,22,23,24,25,4],GPIO.HIGH)
+GPIO.output([17, 18, 27, 22, 23, 24, 25, 4], GPIO.HIGH)
+
 
 class Application:
     def __init__(self, master):
@@ -40,79 +41,79 @@ class Application:
 
     def onQuitButtonClick(self):
         """Quit button callback"""
-        GPIO.cleanup([17,18,27,22,23,24,25,4])   # clean the channels
+        GPIO.cleanup([17, 18, 27, 22, 23, 24, 25, 4])   # clean the channels
         self.master.quit()
 
     def onLed1(self):
         """LED 1"""
         if Led1.get() == 0:
-            GPIO.output(17,GPIO.LOW)
+            GPIO.output(17, GPIO.LOW)
             Led1.set(1)
         else:
-            GPIO.output(17,GPIO.HIGH)
+            GPIO.output(17, GPIO.HIGH)
             Led1.set(0)
 
     def onLed2(self):
         """LED 2"""
         if Led2.get() == 0:
-            GPIO.output(18,GPIO.LOW)
+            GPIO.output(18, GPIO.LOW)
             Led2.set(1)
         else:
-            GPIO.output(18,GPIO.HIGH)
+            GPIO.output(18, GPIO.HIGH)
             Led2.set(0)
 
     def onLed3(self):
         """LED 3"""
         if Led3.get() == 0:
-            GPIO.output(27,GPIO.LOW)
+            GPIO.output(27, GPIO.LOW)
             Led3.set(1)
         else:
-            GPIO.output(27,GPIO.HIGH)
+            GPIO.output(27, GPIO.HIGH)
             Led3.set(0)
 
     def onLed4(self):
         """LED 4"""
         if Led4.get() == 0:
-            GPIO.output(22,GPIO.LOW)
+            GPIO.output(22, GPIO.LOW)
             Led4.set(1)
         else:
-            GPIO.output(22,GPIO.HIGH)
+            GPIO.output(22, GPIO.HIGH)
             Led4.set(0)
 
     def onLed5(self):
         """LED 5"""
         if Led5.get() == 0:
-            GPIO.output(23,GPIO.LOW)
+            GPIO.output(23, GPIO.LOW)
             Led5.set(1)
         else:
-            GPIO.output(23,GPIO.HIGH)
+            GPIO.output(23, GPIO.HIGH)
             Led5.set(0)
 
     def onLed6(self):
         """LED 6"""
         if Led6.get() == 0:
-            GPIO.output(24,GPIO.LOW)
+            GPIO.output(24, GPIO.LOW)
             Led6.set(1)
         else:
-            GPIO.output(24,GPIO.HIGH)
+            GPIO.output(24, GPIO.HIGH)
             Led6.set(0)
 
     def onLed7(self):
         """LED 7"""
         if Led7.get() == 0:
-            GPIO.output(25,GPIO.LOW)
+            GPIO.output(25, GPIO.LOW)
             Led7.set(1)
         else:
-            GPIO.output(25,GPIO.HIGH)
+            GPIO.output(25, GPIO.HIGH)
             Led7.set(0)
 
     def onLed8(self):
         """LED 8"""
         if Led8.get() == 0:
-            GPIO.output(4,GPIO.LOW)
+            GPIO.output(4, GPIO.LOW)
             Led8.set(1)
         else:
-            GPIO.output(4,GPIO.HIGH)
+            GPIO.output(4, GPIO.HIGH)
             Led8.set(0)
 
 if __name__ == '__main__':
